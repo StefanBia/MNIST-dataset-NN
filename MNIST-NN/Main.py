e99642a134f89d07847dd4484610398848310ac1
Stefan Bia <stefanbia0108@gmail.com>
2024-11-15 08:02:47 +0200

    Improve accuracy

--- MNIST-NN/Main.py
+++ MNIST-NN/Main.py
@@ -1,102 +1,39 @@
 import tensorflow as tf
 import numpy as np
-import math
 
-class NaiveDense:
-    def __init__(self, input_size, output_size, activation):
-        self.activation = activation
-
-        w_shape = (input_size, output_size)
-        w_initial_value = tf.random.uniform(w_shape, minval=0, maxval=1e-1)
-        self.W = tf.Variable(w_initial_value)
-
-        b_shape = (output_size,)
-        b_initial_vaule = tf.zeros(b_shape)
-        self.b = tf.Variable(b_initial_vaule)
-
-    def __call__(self, inputs):
-        return self.activation(tf.matmul(inputs,self.W) + self.b)
-
-    @property
-    def weights(self):
-        return [self.W, self.b]
-
-
-class NaiveSequential:
-    def __init__(self, layers):
-        self.layers = layers
-
-    def __call__(self, inputs):
-        x = inputs
-        for layer in self.layers:
-            x = layer(x)
-        return x
-
-    @property
-    def weights(self):
-        weights = []
-        for layer in self.layers:
-            weights += layer.weights
-        return weights
-
-model = NaiveSequential([
-    NaiveDense(input_size=28*28, output_size=512, activation=tf.nn.relu),
-    NaiveDense(input_size=512, output_size=10, activation=tf.nn.softmax)
+# Updated Model Using tf.keras
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(28*28,)),
+    tf.keras.layers.Dense(512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+    tf.keras.layers.Dropout(0.5),  # Dropout for regularization
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
-assert len(model.weights) == 4
 
-class BatchGenerator:
-    def __init__(self, images, labels, batch_size=128):
-        assert len(images) == len(labels)
-        self.index = 0
-        self.images = images
-        self.labels = labels
-        self.batch_size = batch_size
-        self.num_batches = math.ceil(len(images) / batch_size)
+# Compile the model using a more advanced optimizer and loss function
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
 
-    def next(self):
-        images = self.images[self.index : self.index + self.batch_size]
-        labels = self.labels[self.index : self.index + self.batch_size]
-        self.index += self.batch_size
-        return images, labels
+# Loading and preparing the MNIST data using tf.data for efficiency
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape((60000, 28*28)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28*28)).astype('float32') / 255
 
-def one_train_step(model, images_batch, labels_batch):
-    with tf.GradientTape() as tape:
-        predictions = model(images_batch)
-        per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(labels_batch, predictions)
-        average_loss = tf.reduce_mean(per_sample_losses)
-    gradients = tape.gradient(average_loss, model.weights)
-    update_weights(gradients, model.weights)
-    return average_loss
+# Use tf.data for efficient batch processing
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
 
-learning_rate = 0.001
+# Training the model
+model.fit(train_dataset, epochs=10)
 
-def update_weights(gradients, weights):
-    for g, w in zip(gradients, weights):
-        w.assign_sub(g * learning_rate)
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print(f"Test accuracy: {test_accuracy:.2f}")
 
-def fit(model, images, labels, epochs, batch_size=128):
-    for epoch_counter in range(epochs):
-        print(f"Epoch {epoch_counter}")
-    batch_generator = BatchGenerator(images, labels)
-    for batch_counter in range(batch_generator.num_batches):
-        images_batch, labels_batch = batch_generator.next()
-        loss = one_train_step(model, images_batch, labels_batch)
-        if batch_counter % 100 == 0:
-            print(f"Loss at batch {batch_counter}: {loss:.2f}")
-
-from tensorflow.keras.datasets import mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-train_images = train_images.reshape((60000, 28*28))
-train_images = train_images.astype('float32') / 255
-test_images = test_images.reshape((10000, 28 * 28))
-test_images = test_images.astype('float32') / 255
-
-fit(model, train_images, train_labels, epochs=10, batch_size=128)
-
-
-predictions = model(test_images)
-predictions = predictions.numpy()
+# Make predictions and calculate accuracy
+predictions = model.predict(test_images)
 predicted_labels = np.argmax(predictions, axis=1)
 matches = predicted_labels == test_labels
 print(f"Accuracy: {matches.mean():.2f}")
